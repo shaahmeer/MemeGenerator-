a87436d6d8f3d0858b083b1a6e3c5ae3394a34a9
shaahmeer <onlysergio10@gmail.com>
2021-08-25 13:26:30 +0300

    deploy

--- app/src/main/java/com/mirea/nawab/myapplication/MainActivity.java
+++ app/src/main/java/com/mirea/nawab/myapplication/MainActivity.java
@@ -1,11 +1,15 @@
 package com.mirea.nawab.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -13,12 +17,16 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    static ProgressBar simpleProgressBar;
     Button next ,share;
     ImageView imageView;
     @Override
@@ -30,10 +38,15 @@ public class MainActivity extends AppCompatActivity {
         imageView=(ImageView)findViewById(R.id.imageView);
         next=findViewById(R.id.button);
         share=findViewById(R.id.button2);
+        simpleProgressBar = (ProgressBar) findViewById(R.id.simpleProgressBar);
+        loadmeme();
+        simpleProgressBar.setVisibility(View.INVISIBLE);
     }
 
 public void loadmeme()
 {
+
+
     String url="https://meme-api.herokuapp.com/gimme";
     JsonObjectRequest jsonObjectRequest= new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
         @Override
@@ -41,6 +54,7 @@ public void loadmeme()
             try {
                 String url = response.getString("url");
                 Glide.with(MainActivity.this).load(url).into(imageView);
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -57,9 +71,17 @@ public void loadmeme()
 }
 
     public void share(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(intent.EXTRA_TEXT,"funny or not");
+        startActivity(intent);
     }
 
-    public void next(View view) {
-        loadmeme();
+    public void next(View view) { loadmeme();
+simpleProgressBar.setVisibility(View.INVISIBLE);
+
+
     }
+
+
 }
